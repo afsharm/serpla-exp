3d3fd7a8d7c672fbfabf6b0d5524adaf6bf4d3bf
afsharm <afshar.mohebbi@gmail.com>
2023-10-09 11:35:51 +0330

    show errors in json

--- app.js
+++ app.js
@@ -10,4 +10,21 @@ app.use(express.json());
 app.use('/experience', experienceRoutes);
 app.use('/', indexRoutes);
 
+// Custom error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err); // Log the error for debugging purposes
+
+    // Set the response status code
+    res.status(err.status || 500);
+
+    // Send the error response as JSON
+    res.json({
+        error: {
+            message: err.message,
+            // Additional properties if needed
+        },
+    });
+});
+
+
 module.exports = app;
